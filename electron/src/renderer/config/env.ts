a9d88daad1505c26eba8f3dfc4ac34de9c11d5d8
PhuocDang2104 <phuoc.dang2104@gmail.com>
2025-12-06 18:56:56 +0700

    set production true

--- electron/src/renderer/config/env.ts
+++ electron/src/renderer/config/env.ts
@@ -8,7 +8,7 @@
 // - Production (Render): 'https://vnpt-ai-hackathon-meetmate.onrender.com'
 
 // Toggle between local and production
-const IS_PRODUCTION = false;
+const IS_PRODUCTION = true;
 
 export const API_URL = IS_PRODUCTION 
   ? 'https://vnpt-ai-hackathon-meetmate.onrender.com'
